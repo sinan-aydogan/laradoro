d5210ac1dc0434c76fbaa8410b901ecc6ae1087d
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2022-07-10 22:28:51 +0300

    Menü öğelerindeki yazıların seçilebilirliğini engeller

--- resources/js/Components/HeaderMenuItem.vue
+++ resources/js/Components/HeaderMenuItem.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="flex items-center px-3 py-1 hover:bg-indigo-100 hover:text-indigo-700 rounded-lg text-lg cursor-pointer transition duration-200 space-x-1">
+<div class="flex items-center px-3 py-1 hover:bg-indigo-100 hover:text-indigo-700 rounded-lg text-lg cursor-pointer transition duration-200 space-x-1 select-none">
     <slot/>
 </div>
 </template>
