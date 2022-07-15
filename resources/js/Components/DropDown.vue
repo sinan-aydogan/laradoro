847fc8e5dc548861841f253804f798e4a297ec21
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2022-07-15 13:15:48 +0300

    dışarıya tıklanınca dropdown'ın kapanmasını sağlar

--- resources/js/Components/DropDown.vue
+++ resources/js/Components/DropDown.vue
@@ -1,13 +1,18 @@
 <script setup>
 import {ref} from 'vue';
 import HeaderMenuItem from '@/Components/HeaderMenuItem.vue'
+import {onClickOutside} from '@vueuse/core'
+
+const target = ref(null)
+onClickOutside(target, (event) => showContent.value = false)
+
 defineProps({
     content: Array,
     modelValue: ''
 })
 
 const showContent = ref(false)
-const toggle = ()=>{
+const toggle = () => {
     showContent.value = !showContent.value
 }
 </script>
@@ -18,14 +23,19 @@ const toggle = ()=>{
             <slot name="trigger"/>
         </header-menu-item>
 
-        <div v-if="showContent && !modelValue" class="absolute -right-[10rem] border bg-white shadow-2xl p-2 rounded-lg min-w-[22rem]">
-            <slot name="content"></slot>
-        </div>
-
-        <template v-for="i in content" :key="i.id">
-            <div v-if="modelValue === i.id" class="absolute -right-[10rem] border bg-white shadow-2xl p-6 rounded-lg min-w-[22rem]">
-                <slot :name="i.id"/>
+        <div ref="target">
+            <div v-if="showContent && !modelValue"
+                 class="absolute -right-[10rem] border bg-white shadow-2xl p-2 rounded-lg min-w-[22rem]"
+            >
+                <slot name="content"></slot>
             </div>
-        </template>
+
+            <template v-for="i in content" :key="i.id">
+                <div v-if="modelValue === i.id && showContent"
+                     class="absolute -right-[10rem] border bg-white shadow-2xl p-6 rounded-lg min-w-[22rem]">
+                    <slot :name="i.id"/>
+                </div>
+            </template>
+        </div>
     </div>
 </template>
